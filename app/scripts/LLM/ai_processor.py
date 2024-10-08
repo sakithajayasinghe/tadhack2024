6a60a85561e2d08991eaf7801d2ccdc36d9a9ff5
sakitha jayasinghe <125004069+sakithajayasinghe@users.noreply.github.com>
2024-10-08 07:32:33 +0000

    relevent department choosing done

--- app/scripts/LLM/ai_processor.py
+++ app/scripts/LLM/ai_processor.py
@@ -10,9 +10,9 @@ class ai_processor():
             logger.info("=============AI process starting =============")
             genai.configure(api_key=os.environ["GEMINI_API_KEY"])
             model = genai.GenerativeModel(model_name="gemini-1.5-flash")
-
+            appended_data = dept_append(user_input.message)
             result = model.generate_content(
-                user_input.message,
+                appended_data,
                 generation_config=genai.GenerationConfig(
                 response_mime_type="application/json", 
                 response_schema=FilterdOutputFromAI),)
@@ -24,3 +24,14 @@ class ai_processor():
         except Exception as e:
             logger.error(e, exc_info=True)
             raise
+
+def dept_append(user_message):
+    logger.info("text extracting from the text file starting")
+    try:
+        with open('LLM/bank_departments/dept.txt', 'r') as file:
+            file_content = file.read()
+            logger.info("text extracting from the text file finished")
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        raise
+    return f"{user_message}\n" + file_content
--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,13 +1,19 @@
 from pydantic import BaseModel
 from dataclasses import dataclass
-
+from enum import Enum, IntEnum
 
 class UserInput(BaseModel):
     requestId: str
     message: str
 
+class PriorityLevel(str, Enum):
+    HIGH = "HIGH"
+    MEDIUM = "MEDIUM"
+    LOW = "LOW"
+
+
 @dataclass
 class FilterdOutputFromAI():
-    rephrasedQuestion: str
-    briefExplaination: str
-    answerForCustomer: str
+    relventDepartment: str
+    
+
